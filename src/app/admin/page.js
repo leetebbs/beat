e0027def7b1400e868e021028e2730e5004cd8d7
Tebbo <leetebbs@ymail.com>
2024-03-31 15:41:15 +0100

    Create admin page and functionality

---
+++ src/app/admin/page.js
@@ -0,0 +1,30 @@
+"use client"
+import React, { useEffect, useState } from 'react';
+import { Hero } from "@/components/Hero";
+import { ethers } from 'ethers';
+import { Navbar } from "@/components/Navbar";
+import { AdminView } from "@/components/Admin"; // Make sure this path is correct
+import { useAccount } from "wagmi";
+import { adminAddresses } from "@/config/data";
+import Image from "next/image";
+
+export default function AdminPage() {
+    const { address } = useAccount(); 
+    const [isAdmin, setIsAdmin] = useState(false);
+
+
+    useEffect(() => {
+
+        if (adminAddresses.includes(address)) {
+            setIsAdmin(true);
+        }
+        console.log("Address:", address); 
+    }, [address, adminAddresses]); 
+
+ return (
+    <main className="flex min-h-screen flex-col items-center lg:text-black/90 text-black/85">
+      <Navbar />
+      {isAdmin && <AdminView />}
+    </main>
+ );
+}
