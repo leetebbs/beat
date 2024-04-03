98c8d8b54972d534fe6e6a01cc3e5209a59a7587
Tebbo <leetebbs@ymail.com>
2024-04-03 13:50:24 +0100

    resolve button issue and update register

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -6,7 +6,7 @@ export const RegisterView = () => {
     <div className="mt-32 text-black">
       <h1>Register</h1>
       <p>
-        To become an artist on the beats platform please enter your twitter handle.
+      We&apos;re thrilled that you&apos;re interested in joining Beats as an artist! Please fill out the below information to help us verify your identity (to protect our collectors from bots and fake accounts!).
       </p>
       <input type="text" placeholder="Twitter handle" />
       <p>Once we have verified your twitter account you will recieve an exclusive artist NFT that will allow access to the artist portal where you can upload your colletions.</p>
